c8d1cf28b0c34e56dd49896da3311bc71795893f
Vasily Fedoseyev <vasilyfedoseyev@gmail.com>
2014-03-09 02:21:48 +0400

    Make it run on mri too

--- test.rb
+++ test.rb
@@ -1,10 +1,58 @@
+#!/usr/bin/env ruby
 # encoding: utf-8
 
 require 'rubygems'
+require 'benchmark'
 
-require 'protocol_buffers'
-require 'beefcake'
-require 'protobuf'
+
+class BenchmarkBase
+
+  attr_accessor :serialize_result
+  attr_accessor :deserialize_result
+
+  def initialize
+    reset_results!
+  end
+
+  def name
+    self.class.name.gsub(/Benchmark$/, "")
+  end
+
+  def reset_results!
+    @serialize_result = 0
+    @deserialize_result = 0
+  end
+
+  def self.inherited cls
+    self.benchmarks << cls
+  end
+
+  def self.benchmarks
+    @@benchmarks ||= []
+  end
+
+  def available?
+    @available ||= true
+  end
+
+  def not_available!
+    @available = false
+  end
+
+  def prepare!
+    raise "Not available" unless available?
+  end
+
+  def create_object
+    @klass.new :x => 1, :y => 2
+  end
+
+  def serialize iterations=1000, obj=create_object
+  end
+
+  def deserialize iterations=1000, buffer=serialize(1)
+  end
+end
 
 #####################################################################
 ##
@@ -12,33 +60,37 @@ require 'protobuf'
 ##
 #####################################################################
 
-class Variety < ProtocolBuffers::Message
+class ProtocolBuffersBenchmark < BenchmarkBase
 
-  required :int32, :x, 1
-  required :int32, :y, 2
-  optional :string, :tag, 3
+  def prepare!
+    require 'protocol_buffers'
 
-  module Foonum
-    A = 1
-    B = 2
+    @klass = Class.new(ProtocolBuffers::Message) do
+      required :int32, :x, 1
+      required :int32, :y, 2
+      optional :string, :tag, 3
+    end
+  rescue
+    not_available!
+    raise
   end
 
-end
-
-def x1
-  x1 = Variety.new :x => 1, :y => 2
-  s = ""
-
-  startTime = Time.now
-
-  (0...1000).each do |i|
-    x1.serialize(s)
+  def serialize iterations=1000, obj=create_object
+    res = nil
+    iterations.times{
+      res = obj.serialize_to_string
+    }
+    res
   end
 
-  endTimeRPB = Time.now - startTime
-  # puts "--------------------------------------------------------------"
-  # puts "ruby-protocol-buffers, 1000 serialize iterations: #{endTimeRPB} sec"
-  endTimeRPB
+  def deserialize iterations=1000, buffer=serialize(1)
+    obj = nil
+    iterations.times{
+      obj = @klass.parse(buffer)
+    }
+    obj
+  end
+
 end
 
 #####################################################################
@@ -47,93 +99,129 @@ end
 ##
 #####################################################################
 
-class VarietyBeef
-  include Beefcake::Message
+class BeefcakeBenchmark < BenchmarkBase
 
-  required :x, :int32, 1
-  required :y, :int32, 2
-  optional :tag, :string, 3
+  def prepare!
+    require 'beefcake'
 
-  module Foonum
-    A = 1
-    B = 2
+    @klass = Class.new do
+      include Beefcake::Message
+
+      #FIXME: beefcake throws validation error over its own output...
+
+      # required :x, :int32, 1
+      # required :y, :int32, 2
+      optional :x, :int32, 1
+      optional :y, :int32, 2
+      optional :tag, :string, 3
+    end
+  rescue
+    not_available!
+    raise
+  end
+
+  def serialize iterations=1000, obj=create_object
+    res = nil
+    iterations.times{
+      res = obj.encode
+    }
+    res
+  end
+
+  def deserialize iterations=1000, buffer=serialize(1)
+    obj = nil
+    iterations.times{
+      obj = @klass.decode(buffer)
+    }
+    obj
   end
 
 end
 
-def x2
-  x2 = VarietyBeef.new :x => 1, :y => 2
-  s = ""
-
-  startTime = Time.now
-
-  (0...1000).each do |i|
-    x2.encode(s)
-  end
-
-  endTimeBeef = Time.now - startTime
-  # puts "beefcake, 1000 serialize iterations: : #{endTimeBeef} sec"
-  endTimeBeef
-end
 
 #####################################################################
 ##
 ## => protobuf
 ##
 #####################################################################
+class ProtobufBenchmark < BenchmarkBase
 
-class VarietyProto < Protobuf::Message
+  def prepare!
+    require 'protobuf'
 
-  required ::Protobuf::Field::Int32Field, :x, 1
-  required ::Protobuf::Field::Int32Field, :y, 2
-  optional ::Protobuf::Field::StringField, :tag, 3
+    @klass = Class.new(Protobuf::Message) do
+      required ::Protobuf::Field::Int32Field, :x, 1
+      required ::Protobuf::Field::Int32Field, :y, 2
+      optional ::Protobuf::Field::StringField, :tag, 3
+    end
+  rescue
+    not_available!
+    raise
+  end
 
-  module Foonum
-    A = 1
-    B = 2
+  def serialize iterations=1000, obj=create_object
+    res = nil
+    iterations.times{
+      res = obj.serialize_to_string
+    }
+    res
+  end
+
+  def deserialize iterations=1000, buffer=serialize(1)
+    obj = nil
+    iterations.times{
+      obj = @klass.new.parse_from_string(buffer)
+    }
+    obj
   end
 
 end
 
-def x3
-  x3 = VarietyProto.new :x => 1, :y => 2
-  s = ""
-
-  startTime = Time.now
-
-  (0...1000).each do |i|
-    x3.serialize_to_string
-  end
-
-  endTimeProto = Time.now - startTime
-  # puts "protobuf, 1000 serialize iterations: : #{endTimeProto} sec"
-  endTimeProto
-end
 
 #####################################################################
 ##
 ## => java protobuf jar
 ##
 #####################################################################
+class JavaProtobufBenchmark < BenchmarkBase
 
-require 'java'
-require_relative './protobuf-java-2.5.0.jar'
-$CLASSPATH << File.join(Dir.pwd,"target")
-require 'hash_to_proto_converter'
-
-def x4
-  startTime = Time.now
-
-  (0...1000).each do |i|
-    proto = Minecart::HashProtoBuilder.hash_to_proto(
-      com.liquidm.Test::Variety,x: 1,y: 2
-    )
+  def prepare!
+    require 'java'
+    require_relative './protobuf-java-2.5.0.jar'
+    $CLASSPATH << File.join(Dir.pwd,"target")
+    require 'hash_to_proto_converter'
+  rescue
+    not_available!
+    raise
   end
 
-  endTimeJavaProto = Time.now - startTime
-  # puts "java protobuf, 1000 serialize iterations: : #{endTimeJavaProto} sec"
-  endTimeJavaProto
+  def available?
+    RUBY_PLATFORM == "java"
+  end
+
+  if RUBY_PLATFORM == "java"
+    def serialize iterations=1000, obj=create_object
+      string = ""
+      #FIXME: some sane test for java?
+      iterations.times{
+        proto = Minecart::HashProtoBuilder.hash_to_proto(
+          com.liquidm.Test::Variety,x: 1,y: 2
+        )
+      }
+      string
+    end
+
+    def deserialize iterations=1000, buffer=serialize(1)
+      obj = nil
+      iterations.times{
+        raise "NotImplemented"
+      }
+      obj
+    end
+  end
+
 end
+
 
 #####################################################################
 ##
@@ -152,35 +240,71 @@ end
 ##
 #####################################################################
 
-def results x1, x2, x3, x4
-  puts "--------------------------------------------------------------\n"
 
-  puts "protobuf - ruby-protocol-buffers: #{((1 - (x3 / x1)) * 100).round(3)}% faster"
-  puts "protobuf - beefcake #{((1 - (x3 / x2)) * 100).round(3)}% faster"
-  puts "ruby protobuf - java protobuf #{((1 - (x3 / x4)) * 100).round(3)}% faster"
+bms = BenchmarkBase.benchmarks.map{|b| b.new }.select{|b|
+  b.available? && b.prepare!
+  unless b.available?
+    puts "#{b.name} not available"
+  else
+    true
+  end
+}
 
-  puts "--------------------------------------------------------------\n"
+
+serialize_objs = bms.map &:create_object
+
+
+iterations = 100
+iterations.times do |i|
+  bms.each_with_index{|b,i|
+    sub_iterations = 1000
+    b.serialize_result += Benchmark.realtime{
+      b.serialize sub_iterations, serialize_objs[i]
+    }
+  }
+end
+serialize_objs = nil
+
+
+deserialize_objs = bms.map{|b|
+  o = b.serialize(1).to_s
+  puts "#{b.name}: #{o.inspect}"
+  o
+}
+
+
+iterations = 100
+iterations.times do |i|
+  bms.each_with_index{|b,i|
+    sub_iterations = 1000
+    b.deserialize_result += Benchmark.realtime{
+      b.deserialize sub_iterations, deserialize_objs[i]
+    }
+  }
+end
+deserialize_objs = nil
+
+
+def print_results bms, metric
+  res = bms.sort_by{|b|b.send metric}
+  first_res = res.first.send(metric)
+  res.each{|b|
+    comparison = ""
+    current_res = b.send(metric)
+    unless b == res.first
+      comparison = ", #{(current_res/first_res * 100 - 100).round(3)}% slower" unless first_res == 0
+    end
+    puts "  #{'%15.15s' % b.name}: #{b.send(metric).round(3)} sec#{comparison}"
+  }
 end
 
-x1 = x2 = x3 = x4 = 0
-
-ITERATIONS = 10000
-(0...ITERATIONS).each do |i|  
-  x1 += x1(); x2 += x2(); x3 += x3(); x4 += x4();
-end 
-
 puts "===============================================================\n"
-puts "Performance compared to fastest solution, #{ITERATIONS} iterations"
+puts "Performance compared to fastest solution, #{iterations} iteration cycles"
 puts "===============================================================\n"
-puts "Total ellapsed time: "
-puts "  ruby-protocol-buffers: #{x1.round(3)} sec"
-puts "  beefcake:              #{x2.round(3)} sec"
-puts "  ruby protobuf:         #{x3.round(3)} sec"
-puts "  java protobuf:         #{x4.round(3)} sec"
-puts "===============================================================\n"
-x1 /= ITERATIONS; x2 /= ITERATIONS; x3 /= ITERATIONS; x4 /= ITERATIONS;
-puts "protobuf - ruby-protocol-buffers: #{((1 - (x3 / x1)) * 100).round(3)}% faster"
-puts "protobuf - beefcake #{((1 - (x3 / x2)) * 100).round(3)}% faster"
-puts "ruby protobuf - java protobuf #{((1 - (x3 / x4)) * 100).round(3)}% faster"
+puts "Total ellapsed time, serialization: "
+print_results bms, :serialize_result
+puts
+puts "Total ellapsed time, deserialization: "
+print_results bms, :deserialize_result
 
 puts "===============================================================\n"
